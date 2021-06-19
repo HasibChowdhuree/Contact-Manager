72144a635b5ebb8decccb8219f7bc47b75b3834a
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-06-19 16:52:15 +0600

    Created Homepage UI
    Created the user interface of the homepage which includes login and signup form. Also added the css and javascript files in static folder.

--- src/main/java/com/contactmanager/controller/HomeController.java
+++ src/main/java/com/contactmanager/controller/HomeController.java
@@ -1,31 +1,17 @@
 package com.contactmanager.controller;
 
-import com.contactmanager.dao.userrepo;
-import com.contactmanager.entities.Contact;
-import com.contactmanager.entities.User;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
-    @Autowired
-    private userrepo userrep;
 
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
-        User user = new User();
-        user.setName("Hasib Chowdhuree");
-        user.setEmail("hasibchowdhuree@icloud.com");
-
-        Contact contact = new Contact();
-        user.getContacts().add(contact);
-
-        userrep.save(user);
-        System.out.println("Hasib");
-        return "Hello World!";
+    @RequestMapping("/")
+    public String Home(Model model){
+        model.addAttribute("title", "Home - Contact Manager");
+        return "home";
     }
+    
 }
