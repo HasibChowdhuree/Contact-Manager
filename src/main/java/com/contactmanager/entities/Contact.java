82a85d666c2c6a98e68949d992a17b7573406d26
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-07-01 20:51:32 +0600

    Updated Entities
    Updated the entity files - User.java & Contact.java

--- src/main/java/com/contactmanager/entities/Contact.java
+++ src/main/java/com/contactmanager/entities/Contact.java
@@ -1,97 +1,57 @@
 package com.contactmanager.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.Embeddable;
 
-@Entity
-@Table(name = "CONTACT")
+@Embeddable
 public class Contact {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int cid;
-    private String name;
-    private String phone;
-    private String email;
-    private String city;
-    @Column(length = 300)
-    private String description;
-    private String imageurl;
-    @ManyToOne
-    private User user;
-
-    public Contact(){
-        super();
-    }
-
-
-    public int getCid() {
-        return cid;
-    }
-    public void setCid(int cid) {
-        this.cid = cid;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
- 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-
- 
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public String getImageurl() {
-        return imageurl;
-    }
-    public void setImageurl(String imageurl) {
-        this.imageurl = imageurl;
-    }
-
-
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-
+	private String name, email, phone, city, imageurl, description;
+	public Contact() {
+		super();
+	}
+	public Contact(String name, String email, String phone, String city, String imageurl, String description) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.city = city;
+		this.imageurl = imageurl;
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getImageurl() {
+		return imageurl;
+	}
+	public void setImageurl(String imageurl) {
+		this.imageurl = imageurl;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
--- src/main/java/com/contactmanager/entities/User.java
+++ src/main/java/com/contactmanager/entities/User.java
@@ -1,104 +1,86 @@
 package com.contactmanager.entities;
 
-import javax.persistence.CascadeType;
+import java.util.List;
+
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
-
-
-import java.util.List;
-import java.util.ArrayList;
-
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    private String role;
-    private boolean enabled;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	@Column(unique=true)
+	private String email;
+	private String name;
+	private String password;
+	private String role;
     private String imageurl;
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Contact> contacts = new ArrayList<>();
+	@ElementCollection
+    @CollectionTable(
+        name = "contacts",
+        joinColumns = @JoinColumn(name = "id")
+    )
+	private List<Contact> contacts;
+	
 
 
-    public User(){
-        super();
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
 
     
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    
-    public boolean isEnabled() {
-        return enabled;
-    }
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
     public String getImageurl() {
         return imageurl;
     }
     public void setImageurl(String imageurl) {
         this.imageurl = imageurl;
     }
-
-    
     public List<Contact> getContacts() {
         return contacts;
     }
     public void setContacts(List<Contact> contacts) {
         this.contacts = contacts;
     }
+	public String toString() {
+		return email;
+	}
 
 }
