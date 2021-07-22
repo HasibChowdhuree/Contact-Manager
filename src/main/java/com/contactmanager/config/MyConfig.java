c6bfc6bf0bf0fcb0735cb6bbf410d351d44ea0b5
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-07-22 12:56:48 +0600

    Created User Dashboard Page
    Created user_dashboard.html, where only users can enter by signing in using their email and encrypted password with the default Spring Boot login.

--- src/main/java/com/contactmanager/config/MyConfig.java
+++ src/main/java/com/contactmanager/config/MyConfig.java
@@ -36,8 +36,8 @@ public class MyConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/user/**").hasRole("USER")
-		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").defaultSuccessUrl("/user/dashboard").and().csrf().disable();
-		http.csrf().disable();
+		.antMatchers("/**").permitAll().and().formLogin().and().csrf().disable();
+		
 	}
 }
 
