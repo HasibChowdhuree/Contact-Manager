66a2718ee64f8ab0ae38dc94107496c7acab46d7
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-07-22 21:21:24 +0600

    Created Dynamic Navbar for Users
    Created different navbar for /use/** where user's name will be shown with dropdown to log out and edit profile.

--- src/main/java/com/contactmanager/controller/UserController.java
+++ src/main/java/com/contactmanager/controller/UserController.java
@@ -1,13 +1,27 @@
 package com.contactmanager.controller;
 
+import java.security.Principal;
+
+import com.contactmanager.dao.userrepo;
+import com.contactmanager.entities.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private userrepo userRepository;
+
     @RequestMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(Model model, Principal principal){
+        String userName = principal.getName();
+
+        User user = userRepository.getUserByUserName(userName);
+        model.addAttribute("user", user);
         return "normal/user_dashboard";
     }
 }
