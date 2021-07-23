6f1edf8b4240bd573b2f8950bb57ae8b8216eb96
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-07-24 03:28:41 +0600

    Configured adding new contact
    Updated UserController.java and user_add_contact.html so that users can add new contacts.

--- src/main/java/com/contactmanager/controller/UserController.java
+++ src/main/java/com/contactmanager/controller/UserController.java
@@ -3,11 +3,14 @@ package com.contactmanager.controller;
 import java.security.Principal;
 
 import com.contactmanager.dao.userrepo;
+import com.contactmanager.entities.Contact;
 import com.contactmanager.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,8 +32,20 @@ public class UserController {
     public String add_contact(Model model, Principal principal){
         String userName = principal.getName();
         model.addAttribute("title", "Add Contact - Contact Manager");
+        model.addAttribute("contact", new Contact());
         User user = userRepository.getUserByUserName(userName);
         model.addAttribute("user", user);
+        return "normal/user_add_contact";
+    }
+    @PostMapping("/process-new-contact")
+    public String process_new_contact(@ModelAttribute Contact contact, Model model,  Principal principal){
+        model.addAttribute("title", "Add Contact - Contact Manager");
+        String userName = principal.getName(); 
+        User user = userRepository.getUserByUserName(userName);
+        model.addAttribute("user", user);
+
+        user.getContacts().add(contact);
+        this.userRepository.save(user);
         return "normal/user_add_contact";
     }
 }
