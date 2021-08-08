eff46660a95ff62790db4d8a126677a4da5d904f
HasibChowdhuree <hasibchowdhuree@icloud.com>
2021-08-08 21:29:19 +0600

    Configured Password Changing
    Created user_change_password.html and updated UserController.java so that users can change their password from/user/change-password

--- src/main/java/com/contactmanager/controller/UserController.java
+++ src/main/java/com/contactmanager/controller/UserController.java
@@ -10,17 +10,22 @@ import com.contactmanager.entities.User;
 import com.contactmanager.helper.Message;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private userrepo userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @RequestMapping("/dashboard")
     public String dashboard(Model model, Principal principal){
@@ -58,5 +63,43 @@ public class UserController {
 			session.setAttribute("message",new Message(e.getMessage(),"alert-danger"));
 			return "normal/user_add_contact";
 		}
+    }
+    @GetMapping("/change-password")
+    public String change_password(Model model, Principal principal) {
+        model.addAttribute("title", "Change Password - Easy Portfolio");
+    	String userName = principal.getName(); 
+        User user = userRepository.getUserByUserName(userName);
+        model.addAttribute("user", user);
+    	return "normal/user_change_password";
+    }
+    // POST method for change password
+    @PostMapping("/process-change-password")
+    public String process_change_password(Model model, Principal principal, @RequestParam("oldPassword") String oldPassword, @RequestParam("password") String password, @RequestParam("confirmPassword") String confirmPassword, HttpSession session ){
+        model.addAttribute("title", "Change Password - Easy Portfolio");
+    	String userName = principal.getName(); 
+        User user = userRepository.getUserByUserName(userName);
+        model.addAttribute("user", user);
+        try{
+            if(password.length()<6) {
+				throw new Exception("Password length must be at least 6");
+			}
+			if(!password.equals(confirmPassword)) {
+				throw new Exception("Passwords did not match");
+			}
+            if(this.bCryptPasswordEncoder.matches(oldPassword, user.getPassword())){
+                user.setPassword(this.bCryptPasswordEncoder.encode(password));
+                this.userRepository.save(user);
+                session.setAttribute("message",new Message("Password Changed! ","alert-success"));
+            }
+            else{
+                throw new Exception("Enter correct Old Password");
+            }
+            return "normal/user_change_password";
+        }
+        catch(Exception e){
+			e.printStackTrace();
+			session.setAttribute("message",new Message(e.getMessage(),"alert-danger"));
+            return "normal/user_change_password";
+        }
     }
 }
